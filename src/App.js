c56225fb1a6d01df9c7920865b2fa7bb0d464182
Migle Ankstutyte <migle.ankstutyte@ibm.com@Migles-MacBook-Air.local>
2020-03-19 09:53:35 +0200

    Change latte coffee photo

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import * as coffeeItemAction from './actions/coffeeItemAction';
-import './App.scss';
-import getInitialData from './db.js'
+import React from "react";
+import { connect } from "react-redux";
+import * as coffeeItemAction from "./actions/coffeeItemAction";
+import "./App.scss";
+import getInitialData from "./db.js";
 
 class App extends React.Component {
   constructor(props) {
@@ -10,64 +10,66 @@ class App extends React.Component {
     this.state = {
       coffee: [],
       showForm: false,
-      titleError: '',
-      inputKey: Date.now() // to delete file input 
-    }
+      titleError: "",
+      inputKey: Date.now() // to delete file input
+    };
   }
 
   // after button press - show form
   showForm = () => {
-    this.setState((prevState) => {
-      return { showForm: !prevState.showForm }
+    this.setState(prevState => {
+      return { showForm: !prevState.showForm };
     });
-  }
+  };
 
   // handle input change and validate
   handleChange = event => {
     this.setState({ [event.target.name]: event.target.value }, () => {
       const { title } = this.state;
       this.setState({
-        titleError: !title ? 'Title can not be empty' : null,
-      })
+        titleError: !title ? "Title can not be empty" : null
+      });
     });
   };
 
   // submit form
-  handleSubmit = (e) => {
+  handleSubmit = e => {
     e.preventDefault();
     let coffeeItem = {
       title: this.state.title,
       capacity: this.state.capacity,
       price: this.state.price,
       image: this.state.image
-    }
+    };
     this.setState({
-      title: '',
-      capacity: '',
-      price: '',
-      image: '',
+      title: "",
+      capacity: "",
+      price: "",
+      image: "",
       showForm: false,
       inputKey: Date.now()
     });
     this.props.createCoffeeItem(coffeeItem);
-  }
+  };
 
   // reset form
   resetForm = () => {
     this.setState({
       ...this.state,
-      title: '',
-      capacity: '',
-      price: '',
-      image: '',
+      title: "",
+      capacity: "",
+      price: "",
+      image: "",
       inputKey: Date.now()
-    })
-  }
+    });
+  };
 
   addedCoffee(item, index) {
     return (
       <div className="coffee__item" key={index}>
-        <button onClick={(e) => this.deleteCoffeeItem(e, index)}><i className="fa fa-times fa-2x icon"></i></button>
+        <button onClick={e => this.deleteCoffeeItem(e, index)}>
+          <i className="fa fa-times fa-2x icon"></i>
+        </button>
         <img src={item.image} alt="black coffee" />
         <p className="coffee__title">{item.title}</p>
         <hr className="coffee__line" />
@@ -77,10 +79,10 @@ class App extends React.Component {
         </div>
         <div className="coffee__variants">
           <div>{item.capacity} ml</div>
-          <div>{item.price} Eur</div >
+          <div>{item.price} Eur</div>
         </div>
       </div>
-    )
+    );
   }
 
   // delete one coffee item
@@ -90,21 +92,21 @@ class App extends React.Component {
   }
 
   // after add image to input
-  onImageChange = (event) => {
+  onImageChange = event => {
     if (event.target.files && event.target.files[0]) {
       this.setState({
         image: URL.createObjectURL(event.target.files[0])
       });
     }
-  }
+  };
 
   sendDataToLocalStorage = () => {
-    localStorage.setItem('localStorageData', JSON.stringify(getInitialData()));
-  }
+    localStorage.setItem("localStorageData", JSON.stringify(getInitialData()));
+  };
 
   componentDidMount() {
     this.sendDataToLocalStorage();
-    this.props.loadDataFromLocalStorage()
+    this.props.loadDataFromLocalStorage();
   }
 
   render() {
@@ -114,35 +116,96 @@ class App extends React.Component {
         <div className="form">
           <div className="form__title">
             <h3 className="heading">Add New Coffee</h3>
-            <button className="button button--add" onClick={this.showForm}>{!this.state.showForm ? '+' : '-'}</button>
+            <button className="button button--add" onClick={this.showForm}>
+              {!this.state.showForm ? "+" : "-"}
+            </button>
           </div>
-          <form className={this.state.showForm ? 'form__container:active' : 'form__container'} onSubmit={this.handleSubmit} >
+          <form
+            className={
+              this.state.showForm ? "form__container:active" : "form__container"
+            }
+            onSubmit={this.handleSubmit}
+          >
             <label htmlFor="title">
-              <input type="text" id='title' className={`form-control ${this.state.titleError ? 'is-invalid' : ''}`} name="title" placeholder="Title" onChange={this.handleChange} value={this.state.title} />
+              <input
+                type="text"
+                id="title"
+                className={`form-control ${
+                  this.state.titleError ? "is-invalid" : ""
+                }`}
+                name="title"
+                placeholder="Title"
+                onChange={this.handleChange}
+                value={this.state.title}
+              />
               <span>Title</span>
             </label>
-            <div className='error'>{this.state.titleError}</div>
+            <div className="error">{this.state.titleError}</div>
             <label htmlFor="capacity">
-              <input type="number" id='capacity' name="capacity" placeholder="Capacity (ml)" onChange={this.handleChange} value={this.state.capacity} />
+              <input
+                type="number"
+                id="capacity"
+                name="capacity"
+                placeholder="Capacity (ml)"
+                onChange={this.handleChange}
+                value={this.state.capacity}
+              />
               <span>Capacity</span>
             </label>
             <label htmlFor="price">
-              <input type="number" id='price' name="price" placeholder="Price (Eur)" onChange={this.handleChange} value={this.state.price} />
+              <input
+                type="number"
+                id="price"
+                name="price"
+                placeholder="Price (Eur)"
+                onChange={this.handleChange}
+                value={this.state.price}
+              />
               <span>Price</span>
             </label>
             <label htmlFor="files">
-              <input type="file" id="files" onChange={this.onImageChange} key={this.state.inputKey} />
+              <input
+                type="file"
+                id="files"
+                onChange={this.onImageChange}
+                key={this.state.inputKey}
+              />
             </label>
             <div className="form__buttons">
-              <button className="button button--submit" type="submit" disabled={!this.state.title || !this.state.capacity || !this.state.price || !this.state.image}>ADD</button>
-              <button className="button button--reset" type="button" onClick={this.resetForm} disabled={!this.state.title && !this.state.capacity && !this.state.price && !this.state.image}>RESET</button>
+              <button
+                className="button button--submit"
+                type="submit"
+                disabled={
+                  !this.state.title ||
+                  !this.state.capacity ||
+                  !this.state.price ||
+                  !this.state.image
+                }
+              >
+                ADD
+              </button>
+              <button
+                className="button button--reset"
+                type="button"
+                onClick={this.resetForm}
+                disabled={
+                  !this.state.title &&
+                  !this.state.capacity &&
+                  !this.state.price &&
+                  !this.state.image
+                }
+              >
+                RESET
+              </button>
             </div>
           </form>
         </div>
         {Object.values(this.state.coffee).map((item, index) => {
           return (
             <div className="coffee" key={index}>
-              <button onClick={(e) => this.deleteCoffeeItem(e, index)}><i className="fa fa-times fa-2x icon"></i></button>
+              <button onClick={e => this.deleteCoffeeItem(e, index)}>
+                <i className="fa fa-times fa-2x icon"></i>
+              </button>
               <img src={this.state.image} alt="black coffee" />
               <p className="coffee__title">{item.title}</p>
               <hr className="coffee__line" />
@@ -152,29 +215,40 @@ class App extends React.Component {
               </div>
               <div className="coffee__variants">
                 <div>{item.capacity}</div>
-                <div>{item.price}</div >
+                <div>{item.price}</div>
               </div>
             </div>
           );
         })}
-        <div className="coffee">{this.props.allCoffeeItems && this.props.allCoffeeItems.map((item, index) => this.addedCoffee(item, index))}</div>
+        <div className="coffee">
+          {this.props.allCoffeeItems &&
+            this.props.allCoffeeItems.map((item, index) =>
+              this.addedCoffee(item, index)
+            )}
+        </div>
       </div>
-    )
+    );
   }
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     allCoffeeItems: state.allCoffeeItems
-  }
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    createCoffeeItem: coffeeItem => dispatch(coffeeItemAction.createNewCoffeeItem(coffeeItem)),
-    deleteCoffeeItem: index => dispatch(coffeeItemAction.deleteCoffeeItem(index)),
-    loadDataFromLocalStorage: () => dispatch(coffeeItemAction.loadDataFromLocalStorage())
-  }
+    createCoffeeItem: coffeeItem =>
+      dispatch(coffeeItemAction.createNewCoffeeItem(coffeeItem)),
+    deleteCoffeeItem: index =>
+      dispatch(coffeeItemAction.deleteCoffeeItem(index)),
+    loadDataFromLocalStorage: () =>
+      dispatch(coffeeItemAction.loadDataFromLocalStorage())
+  };
 };
 
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
--- src/db.js
+++ src/db.js
@@ -1,27 +1,33 @@
 export default function getInitialData() {
-    const data = [{
-        "image": "https://qph.fs.quoracdn.net/main-qimg-dd7cf1a3dc4e495334f698e8d37ba555",
-        "title": "Black coffee",
-        "price": "1.50",
-        "capacity": "200"
-    },
-    {
-        "image": "https://rawfactoryflavor.com/wp-content/uploads/2015/11/latte.jpg",
-        "title": "Latte",
-        "price": "2.30",
-        "capacity": "200"
-    },
-    {
-        "image": "https://upload.wikimedia.org/wikipedia/commons/d/d8/Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg",
-        "title": "Iced coffee",
-        "price": "2.60",
-        "capacity": "200"
-    },
-    {
-        "image": "https://www.smuckersrms.com/PhotoImage.ashx?photoid=9774&w=600&h=600&mark=1",
-        "title": "Mocha",
-        "price": "2.50",
-        "capacity": "200"
-    }]
-    return data;
+	const data = [
+		{
+			image:
+				'https://qph.fs.quoracdn.net/main-qimg-dd7cf1a3dc4e495334f698e8d37ba555',
+			title: 'Black coffee',
+			price: '1.50',
+			capacity: '200'
+		},
+		{
+			image:
+				'https://cdn.shopify.com/s/files/1/2627/1860/files/how-to-make-the-best-latte_medium.jpg?v=1556825293',
+			title: 'Latte',
+			price: '2.30',
+			capacity: '200'
+		},
+		{
+			image:
+				'https://upload.wikimedia.org/wikipedia/commons/d/d8/Blue_Bottle%2C_Kyoto_Style_Ice_Coffee_%285909775445%29.jpg',
+			title: 'Iced coffee',
+			price: '2.60',
+			capacity: '200'
+		},
+		{
+			image:
+				'https://www.smuckersrms.com/PhotoImage.ashx?photoid=9774&w=600&h=600&mark=1',
+			title: 'Mocha',
+			price: '2.50',
+			capacity: '200'
+		}
+	];
+	return data;
 }
